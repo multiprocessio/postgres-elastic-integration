d5ca1ee3483a923dadc0790d2485da55d6c89c82
Phil Eaton <phil@eatonphil.com>
2022-05-10 13:43:14 +0000

    Add readme

---
+++ README.md
@@ -0,0 +1,28 @@
+# PostgreSQL / Elasticsearch sample data generator
+
+## Setup
+
+### PostgreSQL
+
+Create user and database:
+
+```
+$ sudo su postgres
+$ psql
+> CREATE DATABASE ds_playground;
+> CREATE USER ds_playground WITH PASSWORD 'ds_playground';
+> GRANT ALL ON DATABASE ds_playground TO ds_playground;
+```
+
+Make sure you can log in with user/pass. In `/var/lib/pgsql/data/pg_hba.conf` add:
+
+```
+host ds_playground ds_playground 127.0.0.1/32 md5
+host ds_playground ds_playground ::1/128 md5
+```
+
+### Elasticsearch
+
+```
+$ docker run -d -p 9200:9200 -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.16.3
+```
--- generate.py
+++ generate.py
@@ -1,3 +1,5 @@
+# Depends on psycopg and a postgres database. database: ds_playground, user: ds_playground, pass: ds_playground
+
 import calendar
 import datetime
 import dateutil
@@ -15,6 +17,16 @@ STDEV_RESPONSE_TIME_S = 50
 
 AVG_REQUEST_PER_DAY = 10
 STDEV_REQUEST_PER_DAY = 90
+
+# Not a complete list :)
+LEGAL_ENTITIES_AND_COUNTRIES = {
+    'LLC': ['USA'],
+    'Ltd.': ['Canada', 'USA'],
+    'Inc.': ['Canada', 'USA'],
+    'GmbH': ['Austria', 'Germany'],
+    'AB': ['Sweden'],
+    'SA': ['France'],
+}
 
 
 def http(method, url, body):
@@ -40,6 +52,16 @@ with open('/usr/share/dict/words') as f:
     for line in f:
         words.append(line.strip())
 
+conn = psycopg2.connect("dbname=ds_playground user=ds_playground pass=ds_playground")
+cur = conn.cursor(cursor_factory=PreparingCursor)
+cur.execute("DROP TABLE IF EXISTS ds_playground.customers")
+cur.execute("""CREATE TABLE ds_playground.customers (
+  name TEXT,
+  category TEXT,
+  country TEXT
+)""")
+cur.prepare("INSERT INTO ds_playground.customers VALUES ($1, $2, $3)")
+        
 for customer in range(CUSTOMERS):
     customer_baseline_req_per_day = max(1, random.gauss(AVG_REQUEST_PER_DAY, STDEV_REQUEST_PER_DAY))
     customer_baseline_response_time_s = max(1, random.gauss(AVG_RESPONSE_TIME_S, STDEV_RESPONSE_TIME_S))
@@ -48,10 +70,16 @@ for customer in range(CUSTOMERS):
     month_req_per_day = customer_baseline_req_per_day
     month_response_time_s = customer_baseline_response_time_s
 
+    legal_entity = random.choice(LEGAL_ENTITIES_AND_COUNTRIES.keys())
+    customer_name = random.choice(words) + ' ' + legal_entity
+    customer_category = random.choice(['Buyer', 'Seller'])
+    customer_country = random.choice(LEGAL_ENTITIES_AND_COUNTRIES[category])
+    cur.execute((customer_name, customer_category, customer_country))
+
     for month in range(MONTHS):
         month_req_per_day = max(1, int(month_req_per_day + random.random() * month_req_per_day - month_req_per_day / 2))
         month_response_time_s = max(1, month_response_time_s + random.random() * month_response_time_s - month_response_time_s / 2)
-        
+
         historic_month = (start_of_this_month.month - month - 1) % 12 + 1
         historic_year = start_of_this_month.year + (start_of_this_month.month - month - 1) // 12
         for day in range(calendar.monthrange(historic_year, historic_month)[1]):
@@ -67,4 +95,6 @@ for customer in range(CUSTOMERS):
                 index = f'logs-{historic_year}-{historic_month}-{day+1}'
                 http('POST', f'http://localhost:9200/{index}/_doc', log)
 
-# TODO: store customer info in postgres
+cur.commit()
+cur.close()
+conn.close()
